f098e37d319c85a68f4aad6e64e88d79e59fe861
noevargas5 <153413485+noevargas5@users.noreply.github.com>
2024-07-02 18:06:57 -0400

    Update header.php

---
+++ vista/components/header.php
@@ -0,0 +1,52 @@
+<div class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container">
+          <h2 class="navbar-brand">Illimani Algo  </h2>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="true" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar-collapse collapse show" id="navbarResponsive" style="">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown" data-bs-theme="light">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes" aria-expanded="false">Empleados</a>
+                <div class="dropdown-menu" aria-labelledby="themes">
+                  <a class="dropdown-item" href="../cerulean/">Registrar</a>
+                  <a class="dropdown-item" href="../cosmo/">Modificar</a>
+                  <a class="dropdown-item" href="../cyborg/">Listar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown" data-bs-theme="light">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes" aria-expanded="false">Cargo</a>
+                <div class="dropdown-menu" aria-labelledby="themes">
+                  <a class="dropdown-item" href="../cerulean/">Registrar</a>
+                  <a class="dropdown-item" href="../cosmo/">Modificar</a>
+                  <a class="dropdown-item" href="../cyborg/">Listar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown" data-bs-theme="light">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes" aria-expanded="false">Video Juegos</a>
+                <div class="dropdown-menu" aria-labelledby="themes">
+                  <a class="dropdown-item" href="../cerulean/">Registrar</a>
+                  <a class="dropdown-item" href="../cosmo/">Modificar</a>
+                  <a class="dropdown-item" href="../cyborg/">Listar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown" data-bs-theme="light">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes" aria-expanded="false">Proveedor</a>
+                <div class="dropdown-menu" aria-labelledby="themes">
+                  <a class="dropdown-item" href="../cerulean/">Registrar</a>
+                  <a class="dropdown-item" href="../cosmo/">Modificar</a>
+                  <a class="dropdown-item" href="../cyborg/">Listar</a>
+                </div>
+              </li>
+              <li class="nav-item dropdown" data-bs-theme="light">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="themes" aria-expanded="false">Plataforma</a>
+                <div class="dropdown-menu" aria-labelledby="themes">
+                  <a class="dropdown-item" href="../cerulean/">Registrar</a>
+                  <a class="dropdown-item" href="../cosmo/">Modificar</a>
+                  <a class="dropdown-item" href="../cyborg/">Listar</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
